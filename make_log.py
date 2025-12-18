acd5337ddde20bff3b448d7653a8385a5cee668a
Gagdm <gabriel.alvesgdm@gmail.com>
2025-12-18 20:19:59 -0300

    fix: make_log no recursivity

--- make_log.py
+++ make_log.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import asyncio
 import random
 import os
-from dashboard import *
+import time
 
 # variáveis para auxiliar ajustes, caso necessário
 NUM_SENSORS = 2
@@ -98,58 +98,62 @@ def get_current_data(arc):
 
 if __name__ == "__main__":
 
-    CURRENT_FLOW_TIME, CURRENT_FLOW = get_current_data('flow')
-    CURRENT_TEMP_TIME, CURRENT_TEMPERATURE = get_current_data('temp')
+    while(1):
 
-    try:
-        # caso o arquivo não exista
-        if not os.path.exists(DEBUG_FILE):
-            print("Arquivo não existe!")
+        CURRENT_FLOW_TIME, CURRENT_FLOW = get_current_data('flow')
+        CURRENT_TEMP_TIME, CURRENT_TEMPERATURE = get_current_data('temp')
 
-        # abre o arquivo apenas para leitura
-        debug_file = open(DEBUG_FILE, "a")
+        try:
+            # caso o arquivo não exista
+            if not os.path.exists(DEBUG_FILE):
+                print("Arquivo não existe!")
+
+            # abre o arquivo apenas para leitura
+            debug_file = open(DEBUG_FILE, "a")
+                
+        except Exception as e:
+            print(f"Erro ao ler: {e}")
+
+        try:
+            # caso o arquivo não exista
+            if not os.path.exists(FLOW_DIFFERENCE_FILE):
+                print("Arquivo não existe!")
+
+            # abre o arquivo apenas para leitura
+            flow_file = open(FLOW_DIFFERENCE_FILE, "a")
+
+        except Exception as e:
+            print(f"Erro ao ler: {e}")
+
+        for i in range(NUM_SENSORS):
+
+            # se o sensor de temperatura estiver disconectado
+            if CURRENT_TEMPERATURE[i] == DISC_TEMP:
+                debug_file.write(f"Sensor de temperatura {i+1} disconectado!,{CURRENT_TEMP_TIME}\n")
             
-    except Exception as e:
-        print(f"Erro ao ler: {e}")
+            # se o sensor de vazão estiver disconectado
+            if CURRENT_FLOW[i] == DISC_FLOW:
+                debug_file.write(f"Sensor de vazão {i+1} disconectado!,{CURRENT_TEMP_TIME}\n")
 
-    try:
-        # caso o arquivo não exista
-        if not os.path.exists(FLOW_DIFFERENCE_FILE):
-            print("Arquivo não existe!")
+            # se o sentor de temperatura estiver apontando uma temperatura abaixo da esperada
+            if CURRENT_TEMPERATURE[i] < MIN_TEMP:
+                debug_file.write(f"Sensor de temperatura {i+1} inferiu uma temperatura mais baixa que o normal!,{CURRENT_TEMP_TIME}\n")
 
-        # abre o arquivo apenas para leitura
-        flow_file = open(FLOW_DIFFERENCE_FILE, "a")
+            # se o sentor de temperatura estiver apontando uma temperatura acima da esperada
+            if CURRENT_TEMPERATURE[i] > MAX_TEMP:
+                debug_file.write(f"Sensor de temperatura {i+1} inferiu uma temperatura mais alta que o normal!,{CURRENT_TEMP_TIME}\n")
 
-    except Exception as e:
-        print(f"Erro ao ler: {e}")
+        for i in range(NUM_SENSORS-1):
 
-    for i in range(NUM_SENSORS):
+            # faz a diferença atual e escreve no arquivo
+            dif_current_flow = CURRENT_FLOW[i] - CURRENT_FLOW[i+1]
+            flow_file.write(f"{dif_current_flow},{CURRENT_FLOW_TIME}\n")
 
-        # se o sensor de temperatura estiver disconectado
-        if CURRENT_TEMPERATURE[i] == DISC_TEMP:
-            debug_file.write(f"Sensor de temperatura {i+1} disconectado!,{CURRENT_TEMP_TIME}\n")
-        
-        # se o sensor de vazão estiver disconectado
-        if CURRENT_FLOW[i] == DISC_FLOW:
-            debug_file.write(f"Sensor de vazão {i+1} disconectado!,{CURRENT_TEMP_TIME}\n")
+            # se a diferença da vazão lida nos sensores de vazão for maior que 1
+            if dif_current_flow > DIF_ACCEPT_FLOW:
+                debug_file.write(f"Vazamento detectado no trecho {i+1}!,{CURRENT_TEMP_TIME}\n")
 
-        # se o sentor de temperatura estiver apontando uma temperatura abaixo da esperada
-        if CURRENT_TEMPERATURE[i] < MIN_TEMP:
-            debug_file.write(f"Sensor de temperatura {i+1} inferiu uma temperatura mais baixa que o normal!,{CURRENT_TEMP_TIME}\n")
+        debug_file.close()
+        flow_file.close()
 
-        # se o sentor de temperatura estiver apontando uma temperatura acima da esperada
-        if CURRENT_TEMPERATURE[i] > MAX_TEMP:
-            debug_file.write(f"Sensor de temperatura {i+1} inferiu uma temperatura mais alta que o normal!,{CURRENT_TEMP_TIME}\n")
-
-    for i in range(NUM_SENSORS-1):
-
-        # faz a diferença atual e escreve no arquivo
-        dif_current_flow = CURRENT_FLOW[i] - CURRENT_FLOW[i+1]
-        flow_file.write(f"{dif_current_flow},{CURRENT_FLOW_TIME}\n")
-
-        # se a diferença da vazão lida nos sensores de vazão for maior que 1
-        if dif_current_flow > DIF_ACCEPT_FLOW:
-            debug_file.write(f"Vazamento detectado no trecho {i+1}!,{CURRENT_TEMP_TIME}\n")
-
-    debug_file.close()
-    flow_file.close()
+        time.sleep(30)
